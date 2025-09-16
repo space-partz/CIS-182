cfa5dc8a1fdc983d309eac26f0054ac7ef9ba882
Muhammad Hakeem <125248248+space-partz@users.noreply.github.com>
2025-09-15 21:59:54 -0500

    Completed Lab 2 to show corect output for test scores

--- Lab_2-test_scores-100%/Lab_2-test_scores/test_scores.js
+++ Lab_2-test_scores-100%/Lab_2-test_scores/test_scores.js
@@ -1,20 +1,24 @@
 "use strict";
 
-// get 3 scores from user and add them together
+// get 4 scores from user and add them together
 const score1 = parseInt(prompt("Enter test score"));
 
 const score2 = parseInt(prompt("Enter test score"));
 
 const score3 = parseInt(prompt("Enter test score"));
 
-const total = score1 + score2 + score3;
+const score4 = parseInt(prompt("Enter test score"));
+
+const total = score1 + score2 + score3 + score4;
+
 
 // calculate the average
-const average = parseInt(total/3);
+const average = parseInt(total/4);
 
 // display in browser page
 const html = `<p>Score 1 = ${score1}</p>
     <p>Score 2 = ${score2}</p>
     <p>Score 3 = ${score3}</p>
+    <p>Score 4 = ${score4}</p>
     <p>Average score = ${average}</p>`;
 document.write(html);
