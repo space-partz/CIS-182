26161f429450b95908b0e5ca1a864af48e6bc36c
Muhammad Hakeem <125248248+space-partz@users.noreply.github.com>
2025-09-15 19:23:17 -0500

    Completed Lab 3 - Future Value Calculator.

--- Lab_3-future_value/future_value/future_value.js
+++ Lab_3-future_value/future_value/future_value.js
@@ -17,8 +17,11 @@ while ( isNaN(rate) );
 
 // get number of years - loop until user enters a number
 let years = 0;
+let totalMonths = 0;
 do {
     years = parseInt(prompt("Enter number of years", 10));
+    // Determine total number of months for total number of years
+    totalMonths = years * 12;
 }
 while ( isNaN(years) );
 
@@ -28,13 +31,24 @@ for (let i = 1; i <= years; i++ ) {
     futureValue += futureValue * rate / 100;
 }
 
+//calculate monthly interest value
+let futureMonthlyValue = investment;
+let monthlyInterest = (rate / 100) / 12;
+for (let i = 1; i <= totalMonths; i++ ) {
+    futureMonthlyValue += futureMonthlyValue * monthlyInterest;
+}
+
+
 // display results
 document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
 document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
 document.write(`<p><label>Years:</label> ${years}</p>`);
 document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
 
+// display monthly interest results
+document.write('<h2 style="color: blue;">Future Value with Monthly Interest</h2>');
 document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
 document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
 document.write(`<p><label>Years:</label> ${years}</p>`);
-document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
+document.write(`<p><label>Future Value:</label> ${futureMonthlyValue.toFixed(2)}</p>`);
+
